1316240684303f7fdf66c6acbe303eaf4cbd3504
OscarP69 <oscar.j.pacheco.j.007@gmail.com>
2020-01-28 13:41:35 -0400

    primerenvio

---
+++ resources/views/home.blade.php
@@ -0,0 +1,188 @@
+
+  <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BeeSoft</title>
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
+        crossorigin="anonymous">
+</head>
+
+<body class="hidden">
+    <div class="centrado" id="onload">
+        <div class="lds-facebook"><div></div><div></div><div></div></div>
+    </div>
+    <header>
+        <nav id="nav" class="nav1">
+            <div class="contenedor-nav">
+                <div class="logo">
+                    <img src="img/awd13.png" alt="">
+                </div>
+                <div class="enlaces" id="enlaces">
+                    <a href="#home" id="enlace-home" class="btn-header">Home</a>
+                    <a href="#team" id="enlace-team" class="btn-header">Team</a>
+                    <a href="#about" id="enlace-about us" class="btn-header">About us</a>
+                    <a href="#gallery" id="enlace-gallery" class="btn-header">Gallery</a>
+                    <a href="#contact" id="enlace-contact us" class="btn-header">Contact us</a>
+                    <a href="#feedback" id="enlace-feedback" class="btn-header">Feedback</a>
+                </div>
+                <div class="icono" id="open">
+                    <span>&#9776;</span>
+                </div>
+            </div>
+        </nav>
+        <div class="textos">
+            <h1><b id="tituloColor">B</b>ee<b id="tituloColor">S</b>oft</h1>
+            <h2>Mision</h2>
+            <p>La misión de una empresa. depende de la actividad que la organización realice, así como del entorno en el que se encuentra y de los recursos de los que dispone. Si se trata de una empresa, la misión dependerá del tipo de negocio del que se trate, de las necesidades de la población en ese momento dado y la situación del mercado.
+            </p>
+            <h3>Vision</h3>
+            <p>La visión de una empresa. por otro lado, se refiere a una imagen que la organización plantea a largo plazo sobre cómo espera que sea su futuro, una expectativa ideal de lo que espera que ocurra. La visión debe ser realista pero puede ser ambiciosa, su función es guiar y motivar al grupo para continuar con el trabajo.
+            </p>
+        </div>
+    </header>
+    <main>
+      <section class="team contenedor" id="#team">
+        <h3>Our Team</h3>
+        <p class="after">meet our awesome and creative people</p>
+        <div class="card">
+            <div class="content-card">
+                <div class="people">
+                    <img src="img/khalifa.jpg" alt="">
+                </div>
+                <div class="texto-team">
+                    <h4>Mia Khalifa</h4>
+                    <p>Lorem ipsum dolor sit.</p>
+                </div>
+            </div>
+            <div class="content-card">
+                <div class="people">
+                    <img src="img/abella.jpg" alt="">
+                </div>
+                <div class="texto-team">
+                    <h4>Abella Danger</h4>
+                    <p>Lorem ipsum dolor sit.</p>
+                </div>
+            </div>
+            <div class="content-card">
+                <div class="people">
+                    <img src="img/sasha_grey.jpg" alt="">
+                </div>
+                <div class="texto-team">
+                    <h4>Sasha Grey</h4>
+                    <p>Lorem ipsum dolor sit.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+        <section class="about" id="servicio">
+            <div class="contenedor">
+                <h3 style="color: cornsilk;">About us</h3>
+                <p class="after", style="color: cornsilk;">Always improving ourselves</p>
+                <div class="servicios">
+                    <div class="caja-servicios">
+                        <img src="img/responsive.png" alt="">
+                        <h4>awesomess work</h4>
+                        <p style="color: rgb(0, 100, 100);">Lorem ipsum dolor sit amet consectetur.</p>
+                    </div>
+                    <div class="caja-servicios">
+                        <img src="img/responsive.png" alt="">
+                        <h4>awesomess work</h4>
+                        <p style="color: rgb(0, 100, 100);">Lorem ipsum dolor sit amet consectetur.</p>
+                    </div>
+                    <div class="caja-servicios">
+                        <img src="img/efectos.png" alt="">
+                        <h4>awesomess work</h4>
+                        <p style="color: rgb(0, 100, 100);">Lorem ipsum dolor sit amet consectetur.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="work contenedor" id="trabajo">
+            <h3>Gallery</h3>
+            <p class="after">we make it simple but extraordinary</p>
+            <div class="botones-work">
+                <ul>
+                    <li class="filter active" data-nombre='todos'>Todos</li>
+                    <li class="filter" data-nombre='diseño'>Diseño</li>
+                    <li class="filter" data-nombre='programacion'>Programacion</li>
+                    
+                </ul>
+            </div>
+            <div class="galeria-work">
+                <div class="cont-work programacion">
+                    <div class="img-work">
+                        <img src="img/programacion1.jpeg" alt="">
+                    </div>
+                    <div class="textos-work">
+                        <h4>Programacion</h4>
+                    </div>
+                </div>
+                <div class="cont-work programacion">
+                    <div class="img-work">
+                        <img src="img/programacion2.jpeg" alt="">
+                    </div>
+                    <div class="textos-work">
+                        <h4>Programacion</h4>
+                    </div>
+                </div>
+                <div class="cont-work programacion">
+                    <div class="img-work">
+                        <img src="img/programacion3.jpeg" alt="">
+                    </div>
+                    <div class="textos-work">
+                        <h4>Programacion</h4>
+                    </div>
+                </div>
+                <div class="cont-work diseño">
+                    <div class="img-work">
+                        <img src="img/diseño1.jpeg" alt="">
+                    </div>
+                    <div class="textos-work">
+                        <h4>Diseño</h4>
+                    </div>
+                </div>
+                <div class="cont-work diseño">
+                    <div class="img-work">
+                        <img src="img/diseño2.jpeg" alt="">
+                    </div>
+                    <div class="textos-work">
+                        <h4>Diseño</h4>
+                    </div>
+                </div>
+                <div class="cont-work diseño">
+                    <div class="img-work">
+                        <img src="img/diseño3.jpeg" alt="">
+                    </div>
+                    <div class="textos-work">
+                        <h4>Diseño</h4>
+                    </div>
+                </div>
+                
+            </div>
+        </section>
+    </main>
+    <footer id="contacto">
+        <div class="footer contenedor">
+            <div class="marca-logo">
+                <img src="img/awd13.png" alt="">
+            </div>
+            <div class="iconos">
+                <i class="fab fa-youtube"></i>
+                <i class="fab fa-facebook-square"></i>
+                <i class="fab fa-instagram"></i>
+            </div>
+            <p style="color: white;">Pation and innovation is what make us different</p>
+        </div>
+
+    </footer>
+    <script src="js/jquery.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/filtro.js"></script>
+</body>
+</html>
+
---
+++ resources/views/plantilla/sidebaradministrador.blade.php
@@ -0,0 +1,20 @@
+<div class="sidebar">
+            <nav class="sidebar-nav">
+                <ul class="nav">
+                    <li @click="menu=0" class="nav-item">
+                        <a class="nav-link active" href="#"><i class="icon-speedometer"></i> Escritorio</a>
+                    <li class="nav-item nav-dropdown">
+                        <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-people"></i> Acceso</a>
+                        <ul class="nav-dropdown-items">
+                            <li @click="menu=7" class="nav-item">
+                                <a class="nav-link" href="#"><i class="icon-user"></i> Usuarios</a>
+                            </li>
+                            <li @click="menu=8" class="nav-item">
+                                <a class="nav-link" href="#"><i class="icon-user-following"></i> Roles</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+            <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+        </div>
